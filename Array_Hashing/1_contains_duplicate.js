14c4b77d366a03876b84fba7452ad2704d1077c9
yoonikjyoon <yooni.kj.yoon@gmail.com>
2023-08-05 20:45:47 -0700

    add other solution

--- Array_Hashing/1_contains_duplicate.js
+++ Array_Hashing/1_contains_duplicate.js
@@ -1,6 +1,17 @@
-const continsDuplicate = (nums) => {
+const continsDuplicateA = (nums) => {
   const distinct = new Set(nums);
   return distinct.size != nums.length ? true : false;
 };
 
-console.log(continsDuplicaten[(1, 2, 3, 1)]);
+// console.log(continsDuplicaten[(1, 2, 3, 1)]);
+
+const continsDuplicateB = (nums) => {
+  // array.sort((a, b) => a - b);
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i <= nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+};
